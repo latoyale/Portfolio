a13376433fc83fb87067baf304990be5cc25c2af
latoyale <llegemah@gmail.com>
2016-02-27 22:35:56 -0500

    initial commit

---
+++ reportcard.js
@@ -0,0 +1,13 @@
+// Using reportcard.js with CodeSchool
+$('.report-card.codeschool').reportCard({
+    userName: 'greggpollack',
+    site: 'codeschool',
+    badgesAmount: 7
+});
+
+// Using reportcard.js with Treehouse
+$('.report-card.treehouse').reportCard({
+    userName: 'ryancarson',
+    site: 'treehouse',
+    badgesAmount: 7
+});
